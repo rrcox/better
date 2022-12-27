bd6bdc8aeed87e06d4579a6d01980f38f5576bb4
Rob Cox <rob_cox@binary-development.com>
2022-12-27 01:33:41 -0500

    Switch to prod

--- js/view/home.js
+++ js/view/home.js
@@ -1,6 +1,5 @@
 export default (path) => `
 <div id="logo-container">
-    <!-- <img id="logo" src="/wdd330/better/images/pt.png" alt="physical therapy logo"> -->
     <img id="logo" src="${path}images/pt.png" alt="physical therapy logo">
 </div>
 <h3 id="welcome">Welcome to Being Better Every Day!</h3>
